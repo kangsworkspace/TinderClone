0e9a7617a6d261b84b9dbb868a0b71bbb1d4a177
Healthy <kangsworkspace@gmail.com>
2024-04-10 22:52:30 +0900

    feat: 프로젝트 생성

---
+++ TinderClone/TinderCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderCloneApp.swift
+//  TinderClone
+//
+//  Created by Healthy on 4/10/24.
+//
+
+import SwiftUI
+
+@main
+struct TinderCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
